3a0f27ddb1fffdaa94d151066bc05247419b3d4a
Gabriel Aumala <gabrielfima@live.com>
2016-12-11 14:40:35 -0500

    export html with different width
    set the width of the static page to 960px

--- exportFile.js
+++ exportFile.js
@@ -1,109 +1,127 @@
 const fs = require('fs')
 const temp = require('temp')
 
-function mdToStaticHtml(markdownBody, syntaxHighlightCss, markdownCss, customStyle) {
-  return `<!DOCTYPE html>
-    <html lang="en" class="">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta http-equiv="Content-Language" content="en">
-      <style>
-        ${syntaxHighlightCss}
-      </style>
-      <style>
-        ${markdownCss}
-      </style>
-      <style>
-        .markdown-body {
-          min-width: 200px;
-          max-width: 790px;
-          margin: 0 auto;
-          padding: 30px;
-        }
-        #con-error {
-          position: fixed;
-          top: 0px;
-          right: 0px;
-          padding: 5px;
-          background: white;
-          color: red;
-        }
-      </style>
-      ${customStyle || ''}
-    </head>
-    <body>
-      <div class="wrapper">
-        <div class="container">
-          <div class="repository-with-sidebar repo-container new-discussion-timeline with-full-navigation">
-            <div id="js-repo-pjax-container" class="repository-content context-loader-container">
-              <div id="readme" class="boxed-group flush clearfix announce instapaper_body md">
-                <article class="markdown-body">
-                  ${markdownBody}
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="con-error" style="display:none">Live preview is unavailable</div>
-    </body>
-    </html>`
-}
+function parseHtmlTemplate(html, options) {
+  const lines = html.split(/\r?\n/)
 
-function readCssFiles(markdown, syntax, callback) {
-  fs.readFile(markdown, function (err, markdownData) {
-    if (err) callback(err)
-    else
-      fs.readFile(syntax, function (err, syntaxData) {
-        if (err) callback(err)
-        else
-          callback(null, markdownData.toString('utf-8'), syntaxData.toString('utf-8'))
-      })
-  })
-}
-/*
-function renderPageInPhantom(originPath, destPath, callback) {
-  phantom.create().then(function (phantom) {
-    return phantom.createPage()
-  }).then(function (page) {
-    page.property('viewportSize', { width: 1020, height: 768 })
-    page.property('paperSize', { format: 'A4', orientation: 'portrait', margin: '1cm' })
-    page.property('zoomFactor', 1.5)
-    page.open(originPath).then(function (status) {
-      return page.render(destPath)
-    })
-    .then(function () {
-      console.log('callback page')
-      callback(page)
-    })
-  })
-}
-*/
-function toHtml(dest, content, callback) {
-  const pageWidth = 960
-  const customStyle = `
-    <style>
+  const {
+    highlightCss,
+    markdownBody,
+    markdownCss,
+    noBorder,
+    noJS,
+    pageCss,
+    pageWidth,
+  } = options
+
+  let customStyle = ''
+
+  if(markdownBody) {
+      //in index.html template markdown-body div ends at line 19,
+      //let's add html markdown there
+      lines.splice(19, 0, markdownBody)
+  }
+
+  if(noBorder) customStyle += `
       #readme .markdown-body {
           border: 0px solid #ddd;
       }
-      #js-repo-pjax-container {
-        width: ${pageWidth}px;
-      }
-
-      .markdown-body {
-        min-width: 200px;
-        max-width: ${pageWidth}px;
-      }
-    </style>
     `
-  readCssFiles(__dirname + '/github-markdown.css',
-    __dirname + '/github-syntax-highlight.css',
-    function (readError, markdownCss, syntaxHighlightCss) {
+
+  if(pageWidth) customStyle += `
+        #js-repo-pjax-container {
+          width: ${pageWidth}px;
+        }
+
+        .markdown-body {
+          max-width: ${pageWidth}px;
+        }
+      `
+  if(customStyle.length > 0) {
+      //in index.html template head ends at line 11, let's add new style there
+      lines.splice(11, 0, `<style>${customStyle}</style>`)
+  }
+
+  if(noJS) {
+    //index.html references 2 scripts. this starts at line 9. remove them
+    lines.splice(9, 2)
+  }
+
+  if(pageCss) {
+    //index.html references page css stylesheet in line 8. replace it
+    lines.splice(8, 1, pageCss)
+  }
+
+  if(markdownCss) {
+    //index.html references markdown css stylesheet in line 7. replace it
+    lines.splice(7, 1, markdownCss)
+  }
+
+  if(highlightCss) {
+    //index.html references syntax highlight css stylesheet in line 6. replace it
+    lines.splice(6, 1, markdownCss)
+  }
+
+  return lines.reduce(function (left, right) {
+    return left + '\n' + right
+  })
+
+}
+
+function mdToStaticHtml(template, highlightCss, markdownCss, markdownBody, pageCss) {
+  return parseHtmlTemplate(template, {
+    highlightCss: highlightCss,
+    markdownCss: markdownCss,
+    pageCss: pageCss,
+    markdownBody: markdownBody,
+    noBorder: true,
+    noJS: true,
+    pageWidth: 960,
+  })
+}
+
+function readFilePromise(filepath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filepath, function (err, data) {
+      if (err) reject (err)
+      else resolve (data)
+    })
+  })
+}
+
+function readTemplateFiles(html, highlight, markdown, pageStyle, callback) {
+  let htmlString, highlightString, markdownString, pageStyleString
+  readFilePromise(html)
+    .then(function (data) {
+      htmlString = data.toString('utf-8')
+      return readFilePromise(markdown)
+    }, callback)
+    .then(function (data) {
+      markdownString = data.toString('utf-8')
+      return readFilePromise(pageStyle)
+    }, callback)
+    .then(function (data) {
+      pageStyleString = data.toString('utf-8')
+      return readFilePromise(highlight)
+    }, callback)
+    .then(function (data) {
+      highlightString = data.toString('utf-8')
+      callback(null, htmlString, highlightString, markdownString, pageStyleString)
+    }, callback)
+}
+
+function toHtml(dest, content, callback) {
+  const pageWidth = 960
+
+  readTemplateFiles(__dirname + '/index.html', __dirname + '/github-syntax-highlight.css',
+    __dirname + '/github-markdown.css', __dirname + '/style.css',
+    function (readError, htmlTemplate, syntaxHighlightCss, markdownCss, pageCss) {
       if (readError)
         callback(readError)
       else {
-        const html = mdToStaticHtml(content, markdownCss, syntaxHighlightCss, customStyle)
+        const html = mdToStaticHtml(htmlTemplate,
+          `<style>${syntaxHighlightCss}</style>`, `<style>${markdownCss}</style>`,
+          content, `<style>${pageCss}</style>`)
         fs.writeFile(dest, html, 'utf-8', function (writeError) {
           if(writeError)
             callback(writeError)
@@ -114,24 +132,4 @@ function toHtml(dest, content, callback) {
     })
 }
 
-/*
-function toPdf(dest, content, callback) {
-  readCssFiles(__dirname + '/github-markdown.css',
-    __dirname + '/github-syntax-highlight.css',
-    function (readError, markdownCss, syntaxHighlightCss) {
-      const tempPath = temp.path({suffix: '.htm'})
-      toHtml(tempPath, content, function (htmlError) {
-        if (htmlError) callback(htmlError)
-        else
-          renderPageInPhantom(tempPath, dest, function (page) {
-              //cleanup
-              fs.unlink(tempPath)
-              page.close()
-
-              callback(null)
-          })
-      })
-    })
-}
-*/
 module.exports = toHtml
